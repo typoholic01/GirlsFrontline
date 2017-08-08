db4cbaed99d4d89398324d02a739a075ce99e3cf
bouncewind0105 <bouncewind0105@gmail.com>
2017-08-08 11:09:39 +0900

    Finish feature -Focusing Resouce

---
+++ js/LogisticsSupport.js
@@ -0,0 +1,89 @@
+var humans = new Array();
+var bullets = new Array();
+var foods = new Array();
+var parts = new Array();
+var WatingTimes = new Array();
+var sums = new Array();
+var avarages = new Array();
+
+/////////////////////////////////////////
+//        각 행의 데이터를 가져온다      //
+/////////////////////////////////////////
+function getData() {
+  var rows = $("#myTable2 tbody tr");
+
+  for (var i = 1; i <= rows.length; i++) {
+    humans[i-1] = $("#myTable2 > tbody > tr:nth-child("+i+") > td:nth-child(6)").text();
+    bullets[i-1] = $("#myTable2 > tbody > tr:nth-child("+i+") > td:nth-child(7)").text();
+    foods[i-1] = $("#myTable2 > tbody > tr:nth-child("+i+") > td:nth-child(8)").text();
+    parts[i-1] = $("#myTable2 > tbody > tr:nth-child("+i+") > td:nth-child(9)").text();
+    var time = $("#myTable2 > tbody > tr:nth-child("+i+") > td:nth-child(5)").text();
+    time = time.trim();
+    var hour = time.substring(0,2);
+    console.log("시간: " + hour);
+
+    var minute = time.substring(3,5);
+    console.log("분: " + minute);
+    WatingTimes[i-1] = hour * 60 + minute * 1;
+    console.log("대기시간: " + WatingTimes[i-1]);
+  }
+}
+
+/////////////////////////////////////////
+//        데이터를 계산,출력한다         //
+/////////////////////////////////////////
+function common(choice) {
+  getData();
+
+  for (var i = 0; i < humans.length; i++) {
+    //합계 저장
+    switch (choice) {
+      case "normal":
+        sums[i] = Number(humans[i])+Number(bullets[i])+Number(foods[i])+Number(parts[i])*3;
+        break;
+      case "human":
+        sums[i] = Number(humans[i])*2+Number(bullets[i])+Number(foods[i])+Number(parts[i])*3;
+        break;
+      case "bullet":
+        sums[i] = Number(humans[i])+Number(bullets[i])*2+Number(foods[i])+Number(parts[i])*3;
+        break;
+      case "food":
+        sums[i] = Number(humans[i])+Number(bullets[i])+Number(foods[i])*2+Number(parts[i])*3;
+        break;
+      case "part":
+        sums[i] = Number(humans[i])+Number(bullets[i])+Number(foods[i])+Number(parts[i])*6;
+        break;
+      default :
+        alert("function common 잘못된 호출입니다");
+        break;
+    }
+
+    //평균 저장
+    avarages[i] = Math.round(sums[i]*180/WatingTimes[i]);
+
+    //출력
+    $("#myTable2 > tbody > tr:nth-child("+(i+1)+") > td:nth-child(10)").text(sums[i]);
+    $("#myTable2 > tbody > tr:nth-child("+(i+1)+") > td:nth-child(11)").text(avarages[i]);
+  }
+}
+
+//보통
+function btnNormalFocus() {
+  common("normal");
+}
+//인력 중심
+function btnHumanFocus() {
+  common("human");
+}
+//탄환 중심
+function btnBulletFocus() {
+  common("bullet");
+}
+//식량 중심
+function btnFoodFocus() {
+  common("food");
+}
+//부품 중심
+function btnPartFocus() {
+  common("part");
+}
--- supply-view.php
+++ supply-view.php
@@ -7,77 +7,12 @@
     <link rel="stylesheet" href="./css/table.css">
     <link rel="stylesheet" href="./css/btn.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="./js/sortTable.js" type="text/javascript"></script>
+    <script src="./js/SortTable.js" type="text/javascript"></script>
+    <script src="./js/LogisticsSupport.js" type="text/javascript"></script>
     <script type="text/javascript">
-      var humans = new Array();
-      var bullets = new Array();
-      var foods = new Array();
-      var parts = new Array();
-      var WatingTimes = <?=json_encode($WatingTime) ?>;
-      var sums = new Array();
-      var avarages = new Array();
-
-      /////////////////////////////////////////
-      //        각 행의 데이터를 가져온다      //
-      /////////////////////////////////////////
-      function getData() {
-        var rows = $("#myTable2 tbody tr");
-
-        for (var i = 1; i <= rows.length; i++) {
-          humans[i-1] = $("#myTable2 > tbody > tr:nth-child("+i+") > td:nth-child(6)").text();
-          bullets[i-1] = $("#myTable2 > tbody > tr:nth-child("+i+") > td:nth-child(7)").text();
-          foods[i-1] = $("#myTable2 > tbody > tr:nth-child("+i+") > td:nth-child(8)").text();
-          parts[i-1] = $("#myTable2 > tbody > tr:nth-child("+i+") > td:nth-child(9)").text();
-        }
-      }
-
-      //보통 데이터
-      function btnNormalFocus() {
-        getData();
-
-        for (var i = 0; i < humans.length; i++) {
-          //test
-          // console.log(WatingTimes[i]);
-
-          //합계 저장
-          sums[i] = Number(humans[i])+Number(bullets[i])+Number(foods[i])+Number(parts[i]);
-          console.log("합계: " + sums[i]);
-
-          //평균 저장
-          avarages[i] = Math.round(sums[i]*180/WatingTimes[i]);
-          console.log("평균: " + avarages[i]);
-
-          //출력
-          $("#myTable2 > tbody > tr:nth-child("+(i+1)+") > td:nth-child(10)").text(sums[i]);
-          $("#myTable2 > tbody > tr:nth-child("+(i+1)+") > td:nth-child(11)").text(avarages[i]);
-        }
-      }
-
-      //인력 중심 데이터
-      function btnHumanFocus() {
-        getData();
-        for (var i = 0; i < humans.length; i++) {
-          //합계 저장
-          sums[i] = Number(humans[i])*2+Number(bullets[i])+Number(foods[i])+Number(parts[i]);
-          console.log("합계: " + sums[i]);
-
-          //평균 저장
-          avarages[i] = Math.round(sums[i]*180/WatingTimes[i]);
-          console.log("평균: " + avarages[i]);
-
-          //출력
-          $("#myTable2 > tbody > tr:nth-child("+(i+1)+") > td:nth-child(10)").text(sums[i]);
-          $("#myTable2 > tbody > tr:nth-child("+(i+1)+") > td:nth-child(11)").text(avarages[i]);
-        }
-      }
-      function sumAvgEmpty() {
-        var rows = $("#myTable2 tbody tr");
-        for (var i = 1; i <= rows.length; i++) {
-          $("#myTable2 > tbody > tr:nth-child("+i+") > td:nth-child(10)").empty();
-          $("#myTable2 > tbody > tr:nth-child("+i+") > td:nth-child(11)").empty();
-        }
-      }
-
+      $(document).ready(function () {
+        common("normal");
+      })
     </script>
     <style media="screen">
       .btnGroup {
@@ -89,9 +24,9 @@
     <div class="btnGroup">
       <button type="button" name="button" onclick="btnNormalFocus()">보통</button>
       <button type="button" name="button" onclick="btnHumanFocus()">인력 중심</button>
-      <button type="button" name="button" onclick="sumAvgEmpty()">탄약</button>
-      <button type="button" name="button">식량</button>
-      <button type="button" name="button">부품</button>
+      <button type="button" name="button" onclick="btnBulletFocus()">탄약</button>
+      <button type="button" name="button" onclick="btnFoodFocus()">식량</button>
+      <button type="button" name="button" onclick="btnPartFocus()">부품</button>
     </div>
     <table id="myTable2" class="number-table">
       <thead>
@@ -124,8 +59,8 @@
         <td data-column="탄약"><?php echo $Bullets[$i]; ?></td>
         <td data-column="식량"><?php echo $Foods[$i]; ?></td>
         <td data-column="부품"><?php echo $Parts[$i]; ?></td>
-        <td data-column="총합">we love javascript!</td>
-        <td data-column="효율">we love javascript!</td>
+        <td data-column="총합"><?php echo $Total[$i]; ?></td>
+        <td data-column="효율"><?php echo $Efficiency[$i]; ?></td>
         <td data-column="획득 아이템"><?php echo $ItemCodes[$i]; ?></td>
       </tr>
       <?php } ?>
